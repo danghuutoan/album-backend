2ab4e99031f37824c3b4ca1edf77fd4f1eeebbc2
danghuutoan <danghuutoan2002@gmail.com>
2020-08-26 09:47:57 +0700

    read db url from config file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser')
 const cors = require('cors');
-
+const config = require('config');
 
 const app = express();
 app.use(bodyParser.json());
@@ -17,5 +17,6 @@ require("./startup/routes")(app);
 
 const port = process.env.PORT || 8888;
 app.listen(port, () => {
+    console.log(config.get('name'));
     console.log(`listening on port ${port}`)
 })
--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-
+const config = require('config');
 module.exports = () => {
-    mongoose.connect('mongodb://localhost/investax_album')
+    mongoose.connect(config.get('dbUrl'), { useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => {console.log("connected to mongodb")})
     .catch(err => console.error("couldnot connect to MongoDB", err));
 }
