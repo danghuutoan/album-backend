2cd0323008bd455804db76f104e773843ba043bb
danghuutoan <danghuutoan2002@gmail.com>
2020-08-27 11:30:16 +0700

    adding docker container for mongodb

--- startup/db.js
+++ startup/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 module.exports = () => {
-    mongoose.connect(config.get('dbUrl'), { useNewUrlParser: true, useUnifiedTopology: true})
+    mongoose.connect(`${config.get('dbUrl')}/${config.get('dbName')}`, { useNewUrlParser: true, useUnifiedTopology: true, auth:{authSource:"admin"}, "user":process.env.DB_USER, "pass": process.env.DB_PASSWORD})
     .then(() => {console.log("connected to mongodb")})
     .catch(err => console.error("couldnot connect to MongoDB", err));
 }
