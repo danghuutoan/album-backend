f3fac45a4efe31cba1cff3500bf2ef2d9a1d8b6a
danghuutoan <danghuutoan2002@gmail.com>
2020-08-31 12:01:21 +0700

    adding negative test case for photos/list endpoint

--- tests/integration/photos.test.js
+++ tests/integration/photos.test.js
@@ -20,7 +20,7 @@ describe('/photos/list', () => {
     })
 
     describe('POST /photos/list', () => {
-        it(' should return the list of the available photos', async () => {
+        it(' should return the list of the available photos if the request is valid', async () => {
             const res = await request(server)
                         .post('/photos/list')
                         .send({
@@ -34,11 +34,28 @@ describe('/photos/list', () => {
             expect(res.status).toBe(200);
             expect(res.body).toHaveProperty("message", "OK");
             expect(Array.isArray(res.body.documents)).toBeTruthy();
+            expect(res.body).toHaveProperty("count", 10);
+            expect(res.body).toHaveProperty("skip", 0);
+            expect(res.body).toHaveProperty("limit", 5);
             expect(res.body.documents[0]).toHaveProperty('id');
             expect(res.body.documents[0]).toHaveProperty('album');
             expect(res.body.documents[0]).toHaveProperty('name');
             expect(res.body.documents[0]).toHaveProperty('path');
             expect(res.body.documents.length).toBe(5);
+        })
+
+
+        it(' should return 400 if the request is invalid', async () => {
+            const res = await request(server)
+                        .post('/photos/list')
+                        .send({
+                            'skip': 0,
+                        })
+                        .set('Content-Type', 'application/json')
+                        .set('Accept', 'application/json');
+            
+
+            expect(res.status).toBe(400);
         })
     })
 })
